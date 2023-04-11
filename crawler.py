ec70a7c0f0fc61dd7f7fa1ccd39ce3ea9d2a79ac
ottoalves22 <ottoalves22@gmail.com>
2023-04-11 15:25:10 -0300

    Setting region in search bar but not selecting

--- crawler.py
+++ crawler.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-
-
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class Crawler:
     start_url = "https://finance.yahoo.com/screener/new"
@@ -21,5 +19,24 @@ class Crawler:
                                                         "download.directory_upgrade": True,
                                                         "plugins.always_open_pdf_externally": True
                                                         })
+        # For debugging the browser:
+        chrome_options.add_experimental_option("detach", True)
+
 
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+    def crawl_finances(self, region: str):
+        self.driver.get(self.start_url)
+        # by default Region will include United States. The next action will be removing the default value
+        default_value_bt = self.driver.find_element(By.XPATH, '//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[1]/button')
+        default_value_bt.click()
+
+        add_region_bt = self.driver.find_element(By.XPATH, '//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li/div/div')
+        add_region_bt.click()
+
+        add_region_input = self.driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[1]/div/input')
+        add_region_input.send_keys(region)
+        region_ckbx = self.driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[2]/ul/li/label')
+        self.driver.implicitly_wait(15)
+        region_ckbx.send_keys(Keys.SPACE)
+        
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-if __name__ == 'main':
-    print("Hello World")
+from crawler import Crawler 
+
+crawler = Crawler()
+# The region parameter needs to be according to yahoo finances regions list
+region = 'Brazil'
+crawler.crawl_finances(region)
+print("Hello World")
